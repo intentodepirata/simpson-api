b306404c6e5f9e813676d2d1b5b54e50331881c1
intentodepirata <intentodepirata@hotmail.com>
2023-05-27 22:39:12 +0200

    fix event no used on handlePageChange function

--- src/components/Paginacion/Paginacion.tsx
+++ src/components/Paginacion/Paginacion.tsx
@@ -1,15 +1,18 @@
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 
 interface PaginacionProps {
   setPage: (page: number) => void;
   totalPage: number;
 }
 
-const Paginacion: React.FC<PaginacionProps> = ({ setPage,totalPage }) => {
-    const handlePageChange = (event: React.ChangeEvent<unknown>, page: number) => {
-        setPage(page);
-      };
+const Paginacion: React.FC<PaginacionProps> = ({ setPage, totalPage }) => {
+  const handlePageChange = (
+    _event: React.ChangeEvent<unknown>,
+    page: number
+  ) => {
+    setPage(page);
+  };
 
   return (
     <Stack sx={{ pt: 5 }} spacing={2}>
@@ -18,7 +21,6 @@ const Paginacion: React.FC<PaginacionProps> = ({ setPage,totalPage }) => {
         size="large"
         count={totalPage}
         color="primary"
-        
       />
     </Stack>
   );
